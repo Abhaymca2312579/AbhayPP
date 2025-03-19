a70d7f604c4bda64b3d2723fa027ffd7fd03b16c
Abhaymca2312579 <2k23.mca2312579@gmail.com>
2025-03-19 21:23:29 +0530

    Initial commit for AbhayPP

---
+++ src/components/constants.js
@@ -0,0 +1,50 @@
+
+export const services = [
+    {
+        title: "Technology Enthusiast",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Software Engineer",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+    {
+        title: "Data Manipulator",
+        icon: 'https://csgeeek.github.io/web.png',
+    },
+];
+
+export const name = 'Abhay';
+
+export const experiences = [
+    {
+        'company': 'Amazon Web Service',
+        'role': 'Cloud Practitioner Essentials',
+        'duration': 'February 2025 - Certified',
+        'logo': 'https://static-00.iconduck.com/assets.00/aws-icon-2048x2048-ptyrjxdo.png',
+        'points': [
+            'AWS Cloud Practitioner Essentials certification provides a solid understanding of cloud computing concepts, AWS services, and the AWS Cloud ecosystem📱 .',
+            'It helps you understand the basic functionalities of services like Amazon EC2, S3, and RDS.',
+            'AWS Cloud Practitioner Essentials includes a focus on cloud security and compliance, introducing concepts like shared responsibility models, identity and access management (IAM), and how AWS meets global regulatory and compliance standards,📈.',
+            'The certification helps you understand the AWS pricing model, including cost estimation tools and best practices for cost management.⚙️.'
+        ],
+        'url': 'https://aws.amazon.com/',
+    },
+    {
+        'company': 'FUTURE INTERNS',
+        'role': 'Full Stack Web Developer',
+        'duration': 'Jan 2025 - Feb 2025',
+        'logo': 'https://media.licdn.com/dms/image/v2/D560BAQFD35K5nrk-MQ/company-logo_200_200/company-logo_200_200/0/1720451756839?e=1749081600&v=beta&t=D_63l19TPwdnojlI4Z2eJexUnGV3MxxhSU-JAepBRvA',
+        'points': [
+            'As a Full Stack Web Developer, I worked on both frontend and backend technologies to build dynamic, responsive, and scalable web applications, utilizing frameworks like React, Angular, and Node.js, alongside databases like MySQL and MongoDB.',
+            'I collaborated with cross-functional teams to design, implement, and optimize user interfaces and server-side logic, ensuring seamless integration, high performance, and an overall exceptional user experience across web platforms.📱.'
+        ],
+        'url': 'https://futureintern.in/',
+    },
+]
+
+
+
+export const EMAIL_JS_SERVICE_ID = process.env.REACT_APP_EMAIL_JS_SERVICE_ID;
+export const EMAIL_JS_TEMPLATE_ID = process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID;
+export const EMAIL_JS_PUBLIC_KEY = process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY;
